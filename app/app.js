55b2e4cda33dccf9be68b3bf9f84f342680e4498
PengJuan-AI <pj2018022187@126.com>
2024-08-23 15:13:01 +0800

    update app.js

--- app/app.js
+++ app/app.js
@@ -1,12 +1,14 @@
 // app.js
 
 import express from 'express';
-import homeRoute from './routes/homeRoute.js'
+import cors from 'cors';
+import homeRoute from './routes/homeRoute.js';
 
 const app = express();
 const port =3000;
 
 app.use(express.json()); // More modern approach than using the body-parser library. it is now not required.
+app.use(cors())
 
 app.use('/', homeRoute);
 app.listen(port, () => {
