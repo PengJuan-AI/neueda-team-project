8d383b938eeec808e82507e86ac2aa7da99893dc
Kaiyi Sun <60586553+KaiyiSss@users.noreply.github.com>
2024-08-29 01:42:11 -0700

    Update portfolio_db.sql

--- portfolio_db.sql
+++ portfolio_db.sql
@@ -27,12 +27,12 @@ CREATE TABLE Stock (
 -- Corrected INSERT INTO statements
 INSERT INTO crypto (symbol, corp, price, quantity, change_percent)
 VALUES 
-('BTC-USD', 'Bitcoin USD', 60383.93, 1, 0.29),
-('ETH-USD', 'Ethereum USD', 2673.99, 1, 0.29),
+-- ('BTC-USD', 'Bitcoin USD', 60383.93, 1, 0.29)
+-- ('ETH-USD', 'Ethereum USD', 2673.99, 1, 0.29),
 ('BNB-USD', 'BNB USD', 579.89, 1, 1.33), -- Changed quantity to integer
 ('SOL-USD', 'Solana USD', 145.15, 1, 1.96), -- Corrected symbol to SOL-USD and fixed quantity
-('USDC-USD', 'USD Coin USD', 1.0000, 1, 0.0016), -- Added missing quantity value
-('XRP-USD', 'XRP USD', 0.598900, 1, -0.2403), -- Added missing quantity value
+-- ('USDC-USD', 'USD Coin USD', 1.0000, 1, 0.0016), -- Added missing quantity value
+-- ('XRP-USD', 'XRP USD', 0.598900, 1, -0.2403), -- Added missing quantity value
 ('TON-USD', 'Toncloin USD', 6.5800, 1, 1.280), -- Fixed quantity to 1
 ('CREAM-USD', 'Cream USD', 20.912, 1, 1.280), -- Fixed quantity to 1
 ('TRB-USD', 'Tellor USD', 57.328, 1, 0.409); -- Fixed quantity to 1
